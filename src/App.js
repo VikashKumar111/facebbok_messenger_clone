497e961f6c4219966abc3d6ed9b68d589d34c377
Vikash Kumar <vikasthakur9310@gmail.com>
2023-06-25 11:49:22 +0530

    add

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
-  console.log(input);
+  const [messeges, setMesseges] = useState(["hello", "Welcome!", "Join our awesome newsletter!"]);
+  // console.log(input);
+  console.log(messeges);
+  
+  const sendMessage = (event) => {
+    setMesseges([...messeges, input]);
+    setInput('');
+  }
   return (
     <div className="App">
       <h1> Hello programmmers community </h1>
 
       <input value={input} onChange={event => setInput(event.target.value)} />
-      <button>Send message</button>
+      <button onClick={sendMessage}>Send message</button>
     </div>
   );
 }
